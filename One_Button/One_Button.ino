bf379dca8dc3d19ea77dc60f63f5bd22292e9cd1
mikaelaehunter <mhunter@tcd.ie>
2024-06-20 15:53:14 +0100

    delete use of keyboard library
    https://docs.arduino.cc/built-in-examples/digital/StateChangeDetection/

--- One_Button/One_Button.ino
+++ One_Button/One_Button.ino
@@ -11,40 +11,24 @@ void setup() {
   //declare the buttons as input_pullup
   pinMode(down, INPUT_PULLUP);
   Serial.begin(9600);  
-  Keyboard.begin();
 }
 
 void loop() {
 
-  //downArrow();
   edgeDetection();
 
-}
-
-void downArrow(){
-  if (digitalRead(down) == LOW)                     //Checking if the first switch has been pressed
-  {
-    Keyboard.press(KEY_DOWN_ARROW);
-    delay(100);
-    Keyboard.release(KEY_DOWN_ARROW);
-    delay(100);
-  }
 }
 
 void edgeDetection(){
   buttonState = digitalRead(down);
   if (buttonState != lastButtonState){
     if (buttonState == LOW){
-      Keyboard.press(KEY_DOWN_ARROW);
-      delay(10);
       buttonPushCounter++;
-      Keyboard.println("on");
-      Keyboard.print("number of button pushes: ");
-      Keyboard.println(buttonPushCounter);
+      Serial.println("on");
+      Serial.print("number of button pushes: ");
+      Serial.println(buttonPushCounter);
     } else {
-        Keyboard.release(KEY_DOWN_ARROW);
-        delay(10);
-        Keyboard.println("off");
+        Serial.println("off");
     }
     delay(50);
     }
