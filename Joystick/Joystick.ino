ab1c8b86f39b0926b60ef94cb54659cdf116f83d
mikaelaehunter <mhunter@tcd.ie>
2024-06-21 11:07:48 +0100

    Delete mouseButton

--- Joystick/Joystick.ino
+++ Joystick/Joystick.ino
@@ -1,8 +1,7 @@
-#include "Mouse.h"
+#include "Mouse.h" // include mouse library
 
 // set pin numbers for switch, joystick axes, and LED:
 const int switchPin = 9;    // switch to turn on and off mouse control
-const int mouseButton = 8;  // input pin for the mouse pushButton
 const int xAxis = A0;       // joystick X axis
 const int yAxis = A1;       // joystick Y axis
 const int ledPin = 10;       // Mouse control LED
@@ -19,71 +18,42 @@ int lastSwitchState = LOW;   // previous switch state
 void setup() {
   pinMode(switchPin, INPUT);  // the switch pin
   pinMode(ledPin, OUTPUT);    // the LED pin
-  // take control of the mouse:
-  Mouse.begin();
+  
+  Mouse.begin();              // take control of the mouse:
 }
 
 void loop() {
-  // read the switch:
-  int switchState = digitalRead(switchPin);
-  // if it's changed and it's high, toggle the mouse state:
-  if (switchState != lastSwitchState) {
+  
+  int switchState = digitalRead(switchPin);             // read the switch:
+  
+  if (switchState != lastSwitchState) {                 // if it's changed and it's high, toggle the mouse state:
     if (switchState == HIGH) {
       mouseIsActive = !mouseIsActive;
-      // turn on LED to indicate mouse state:
-      digitalWrite(ledPin, mouseIsActive);
+      digitalWrite(ledPin, mouseIsActive);              // turn on LED to indicate mouse state:
     }
   }
-  // save switch state for next comparison:
-  lastSwitchState = switchState;
 
-  // read and scale the two axes:
-  int xReading = readAxis(A0);
+  lastSwitchState = switchState;                        // save switch state for next comparison:
+
+  int xReading = readAxis(A0);                          // read and scale the two axes:
   int yReading = readAxis(A1);
 
-  // if the mouse control state is active, move the mouse:
-  if (mouseIsActive) {
+  if (mouseIsActive) {                                  // if the mouse control state is active, move the mouse:
     Mouse.move(xReading, yReading, 0);
-  }
-
-  // read the mouse button and click or not click:
-  // if the mouse button is pressed:
-  if (digitalRead(mouseButton) == HIGH) {
-    // if the mouse is not pressed, press it:
-    if (!Mouse.isPressed(MOUSE_LEFT)) {
-      Mouse.press(MOUSE_LEFT);
-    }
-  }
-  // else the mouse button is not pressed:
-  else {
-    // if the mouse is pressed, release it:
-    if (Mouse.isPressed(MOUSE_LEFT)) {
-      Mouse.release(MOUSE_LEFT);
-    }
   }
 
   delay(responseDelay);
 }
 
-/*
-  reads an axis (0 or 1 for x or y) and scales the analog input range to a range
-  from 0 to <range>
-*/
-
-int readAxis(int thisAxis) {
-  // read the analog input:
-  int reading = analogRead(thisAxis);
-
-  // map the reading from the analog input range to the output range:
-  reading = map(reading, 0, 1023, 0, range);
-
-  // if the output reading is outside from the rest position threshold, use it:
-  int distance = reading - center;
+int readAxis(int thisAxis) {                              //reads an axis (0 or 1 for x or y) and scales the analog input range to a range from 0 to <range>
+  
+  int reading = analogRead(thisAxis);                     // read the analog input:
+  reading = map(reading, 0, 1023, 0, range);              // map the reading from the analog input range to the output range:
+  int distance = reading - center;                        // if the output reading is outside from the rest position threshold, use it:
 
   if (abs(distance) < threshold) {
     distance = 0;
   }
 
-  // return the distance for this axis:
-  return distance;
+  return distance;                                        // return the distance for this axis:
 }
