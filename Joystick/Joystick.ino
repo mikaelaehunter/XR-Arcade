9ecd109b8a003998c5ef00b6747372bddd4d6f2d
mikaelaehunter <mhunter@tcd.ie>
2024-06-21 16:06:48 +0100

    Deletes joystick button and LED

--- Joystick/Joystick.ino
+++ Joystick/Joystick.ino
@@ -1,59 +1,30 @@
-#include "Mouse.h" // include mouse library
+#include <Mouse.h>
 
-// set pin numbers for switch, joystick axes, and LED:
-const int switchPin = 9;    // switch to turn on and off mouse control
-const int xAxis = A0;       // joystick X axis
-const int yAxis = A1;       // joystick Y axis
-const int ledPin = 10;       // Mouse control LED
+int xPin = A0;  // Analog output of horizontal joystick pin
+int yPin = A1;  // Analog output of vertical joystick pin
 
-// parameters for reading the joystick:
-int range = 12;             // output range of X or Y movement
-int responseDelay = 5;      // response delay of the mouse, in ms
-int threshold = range / 4;  // resting threshold
-int center = range / 2;     // resting position value
+int yZero, xZero;  // Stores the initial value of each axis, usually around 512
+int yValue, xValue;  // Stores current analog output of each axis
+const int sensitivity = 100;  // Higher sensitivity value = slower mouse, should be <= about 500
+int mouseClickFlag = 0;
+int invertMouse = 1; //Value to invert joystick based on orientation
 
-bool mouseIsActive = false;  // whether or not to control the mouse
-int lastSwitchState = LOW;   // previous switch state
-
-void setup() {
-  pinMode(switchPin, INPUT);  // the switch pin
-  pinMode(ledPin, OUTPUT);    // the LED pin
-  
-  Mouse.begin();              // take control of the mouse:
+void setup()
+{
+  pinMode(xPin, INPUT);  // Set both analog pins as inputs
+  pinMode(yPin, INPUT);
+  delay(1000);  // short delay to let outputs settle
+  yZero = analogRead(yPin);  // get the initial values
+  xZero = analogRead(xPin);  // Joystick should be in neutral position when reading these
 }
 
-void loop() {
-  
-  int switchState = digitalRead(switchPin);             // read the switch:
-  
-  if (switchState != lastSwitchState) {                 // if it's changed and it's high, toggle the mouse state:
-    if (switchState == HIGH) {
-      mouseIsActive = !mouseIsActive;
-      digitalWrite(ledPin, mouseIsActive);              // turn on LED to indicate mouse state:
-    }
-  }
+void loop()
+{
+  yValue = analogRead(yPin) - yZero;  // read vertical offset
+  xValue = analogRead(xPin) - xZero;  // read horizontal offset
 
-  lastSwitchState = switchState;                        // save switch state for next comparison:
-
-  int xReading = readAxis(A0);                          // read and scale the two axes:
-  int yReading = readAxis(A1);
-
-  if (mouseIsActive) {                                  // if the mouse control state is active, move the mouse:
-    Mouse.move(xReading, yReading, 0);
-  }
-
-  delay(responseDelay);
-}
-
-int readAxis(int thisAxis) {                              //reads an axis (0 or 1 for x or y) and scales the analog input range to a range from 0 to <range>
-  
-  int reading = analogRead(thisAxis);                     // read the analog input:
-  reading = map(reading, 0, 1023, 0, range);              // map the reading from the analog input range to the output range:
-  int distance = reading - center;                        // if the output reading is outside from the rest position threshold, use it:
-
-  if (abs(distance) < threshold) {
-    distance = 0;
-  }
-
-  return distance;                                        // return the distance for this axis:
+  if (yValue != 0)
+    Mouse.move(0, (invertMouse * (yValue / sensitivity)), 0); // move mouse on y axis
+  if (xValue != 0)
+    Mouse.move((invertMouse * (xValue / sensitivity)), 0, 0); // move mouse on x axis
 }
