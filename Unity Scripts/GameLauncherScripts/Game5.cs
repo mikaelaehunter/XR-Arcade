b6052861a0319f4bce39e044f99936a14f30175e
mikaelaehunter <mhunter@tcd.ie>
2024-07-19 16:41:10 +0100

    Create Game5.cs

---
+++ Unity Scripts/GameLauncherScripts/Game5.cs
@@ -0,0 +1,71 @@
+using System.Diagnostics;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using CodeMonkey.Utils;
+
+public class Game5 : MonoBehaviour {
+
+    private Process process;
+    private Transform messageTransform;
+    private Text messageText;
+    private Button_UI Button5;
+
+    private void Awake() {
+        Application.runInBackground = false;
+        Application.targetFrameRate = 100;
+
+        Button5 = transform.Find("Btn5").GetComponent<Button_UI>();
+        Button5.ClickFunc = () => {
+            // Launch Game!
+            string path = Application.dataPath + "/../Builds/Students/john/Wildgoose_John_XR_Assign1/Wildgoose_John_XR_Assign1_Win64-bit/XR Assignment 1.exe";
+            process = Process.Start(path);
+            ShowProcessLaunchedMessage("Launching Escape the Abandoned City...");
+        };
+
+        messageTransform = transform.Find("message");
+        messageText = messageTransform.Find("Text").GetComponent<Text>();
+        HideProcessLaunchedMessage();
+    }
+
+    private void Update() {
+        if (process != null && process.HasExited) {
+            // Process exited!
+            process = null;
+            HideProcessLaunchedMessage();
+        }
+
+        if (Input.GetKeyDown(KeyCode.Escape)) {
+            // Close the application
+            Application.Quit();
+        }
+         if (Input.GetKeyDown(KeyCode.Return)) {
+            // Simulate button click when Enter key is pressed
+            Button5.ClickFunc();
+        }
+
+        if (Input.GetKeyDown(KeyCode.RightArrow)) {
+            // Simulate button hover when Right Arrow key is pressed
+            Button5.ManualOnPointerEnter();
+        }
+
+        if (Input.GetKeyDown(KeyCode.LeftArrow)) {
+            // Simulate mouse hover exit when Left Arrow key is pressed
+            Button5.ManualOnPointerExit();
+        }
+    }
+
+    private void ShowProcessLaunchedMessage(string message) {
+        messageTransform.gameObject.SetActive(true);
+        messageText.text = message;
+    }
+
+    private void HideProcessLaunchedMessage() {
+        messageTransform.gameObject.SetActive(false);
+    }
+
+
+}
+
+
